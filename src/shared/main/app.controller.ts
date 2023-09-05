4cd866e6500c4e6350dd2fbaab4a3e3d15b50622
LyoDekken <humberto.araripe12@gmail.com>
2023-09-05 14:53:05 -0300

    feat: "rename"

--- src/shared/main/app.controller.ts
+++ src/shared/main/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Routes } from 'src/common/constant/constants';
 
-@ApiTags('status')
+@ApiTags(Routes.STATUS)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('status')
+  @Get('test')
   serverIsRunning(): string {
     return this.appService.serverIsRunning();
   }
