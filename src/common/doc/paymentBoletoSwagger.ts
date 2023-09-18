9fd22d1ecf79492e150e8c82df4115485336f0cc
LyoDekken <humberto.araripe12@gmail.com>
2023-09-18 13:46:17 -0300

    feat: "boleto is optional"

--- src/common/doc/paymentBoletoSwagger.ts
+++ src/common/doc/paymentBoletoSwagger.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsString,
+  IsOptional,
   IsNumber,
   IsArray,
   ValidateNested,
@@ -11,6 +12,7 @@ import { Type } from 'class-transformer';
 class DivisaoDTO {
   @ApiProperty()
   @IsString()
+  @IsOptional()
   identificador: string;
 
   @ApiProperty()
@@ -23,20 +25,24 @@ class DivisaoDTO {
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   nomeservico: string;
 }
 
 export class ClienteDTO {
   @ApiProperty()
   @IsString()
+  @IsOptional()
   urlretorno: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   chavepessoa: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   chaveerp: string;
 
   @ApiProperty()
@@ -49,10 +55,12 @@ export class ClienteDTO {
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   nome: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   identificador: string;
 
   @ApiProperty()
@@ -61,46 +69,57 @@ export class ClienteDTO {
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   telefone: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   uf: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   cidade: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   logradouro: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   bairro: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   numero: string | null;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   complemento: string | null;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   cep: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   nomebeneficiario: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   cpfbeneficiario: string;
 
   @ApiProperty()
   @IsString()
+  @IsOptional()
   boleto_datavencimento: string;
 
   @ApiProperty()
